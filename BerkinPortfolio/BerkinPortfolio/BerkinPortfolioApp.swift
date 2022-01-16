2ff1c49a8367939940f1033e91c4d4cfcdbe1007
BerkinSili <berkinsili@gmail.com>
2022-01-16 12:19:03 +0300

    Created a data model, DO NOT FORGET TO ADD iCLOUD CAP ONCE YOU HAVE PAID ACCOUNT

---
+++ BerkinPortfolio/BerkinPortfolio/BerkinPortfolioApp.swift
@@ -0,0 +1,27 @@
+//
+//  BerkinPortfolioApp.swift
+//  BerkinPortfolio
+//
+//  Created by Berkin Sili on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct BerkinPortfolioApp: App {
+    
+    @StateObject var dataController: DataController
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
