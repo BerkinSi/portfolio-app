76f04aa3b1a32ce84fa8e14c8f33034a3c1867b1
BerkinSili <berkinsili@gmail.com>
2022-01-20 16:35:42 +0300

    Create lists and a tabbar view

---
+++ BerkinPortfolio/BerkinPortfolio/ProjectsView.swift
@@ -0,0 +1,45 @@
+//
+//  ProjectsView.swift
+//  BerkinPortfolio
+//
+//  Created by Berkin Sili on 19.01.2022.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+    let showClosedPorjects: Bool
+    let projects: FetchRequest<Project>
+    
+    init(showClosedPorjects: Bool) {
+        self.showClosedPorjects = showClosedPorjects
+        projects = FetchRequest<Project>(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)], predicate: NSPredicate(format: "closed = %d", showClosedPorjects))
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(projects.wrappedValue) { project in
+                    Section(header: Text(project.title ?? "")) {
+                        ForEach(project.items?.allObjects as? [Item] ?? []) { item in
+                            Text(item.title ?? "")
+                            
+                        }
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle(showClosedPorjects ? "Closed Projects" : "Open Projects")
+        }
+    }
+}
+
+struct ProjectsView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+     
+    static var previews: some View {
+        ProjectsView(showClosedPorjects: false)
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+    }
+}
